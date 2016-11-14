a1ed8d322c3d5dce617430d9ed1616aea4df4996
Nana Baah Akuffu <Nana Baah Akuffu>
2016-11-14 17:06:51 +0000

    Edited the user_query

--- user_query.php
+++ user_query.php
@@ -21,7 +21,7 @@
 		<div class="col-sm-6">
 			<div class="panel panel-default">
 				<div class="panel-heading"><h4>Set User Priveleges</h4></div>
-				<div class="panel-body"> You have not set the access priveleges of, <b><?php $user_name ?></b>.
+				<div class="panel-body"> You have not set the access priveleges of, <b><?php echo $user_name ?></b>.
 					You need to set this before he/she can be active. <br><br> If you want to set them now please click <b>Yes</b>, otherwise click <b>No</b>. <br><br>
 					<?php echo "<a class='btn btn-primary' style='width:15%' href=user_levels.php?level={$new_id}&upd={$up_3}> Yes </a>"; ?>
 					<a class="btn btn-primary" style="width:15%" href="user_page.php"> No </a>
